4433584994d0dc2958ccf6881d14d2ce55be4d0e
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-03-16 22:24:04 +0530

    Updating the Checks of LongURL

--- src/components/UrlShortner.jsx
+++ src/components/UrlShortner.jsx
@@ -7,11 +7,15 @@ const UrlShortner = () => {
   const textRef = useRef();
 
   // state for short url
-  const [shortURL, setShortURL] = useState("adeeb-copyupwala");
+  const [shortURL, setShortURL] = useState("");
   const [longURL, setLongURL] = useState("");
 
   // function to shorten-url
   const shortenUrl = async () => {
+    if(longURL===""){
+      toast.error("It Can't be Empty")
+      return ;
+    }
     const response = await fetch(
       `https://tinyurl.com/api-create.php?url=${longURL}`
     );
@@ -27,10 +31,10 @@ const UrlShortner = () => {
   };
   return (
     <div className="w-[100%] md:w-[50%] font-[Poppins] flex flex-col items-center">
-      <div className="flex gap-4 md:flex-row flex-col">
+      <div className="md:w-[100%] flex gap-4 md:flex-row flex-col items-center">
         <input
           type="text"
-          className="w-[90%] px-4 py-2 text-black"
+          className="w-[100%] px-4 py-2 text-black"
           onChange={(e) => setLongURL(e.target.value)}
           value={longURL}
         />
@@ -38,7 +42,7 @@ const UrlShortner = () => {
           onClick={() => {
             shortenUrl();
           }}
-          className="max-md:w-[20%] bg-blue-500 text-white rounded-lg px-4 py-2"
+          className="bg-blue-500 text-white rounded-lg px-4 py-2"
         >
           Shorten URL
         </button>
