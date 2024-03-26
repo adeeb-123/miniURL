35c75fdd760912d8647eb67bb2550f1335d639d9
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-03-26 18:07:34 +0530

    Basic changes in global/Footer

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -8,15 +8,15 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 const Footer = () => {
     return (
         <div className='w-[100%] py-4'>
-            <div className='w-[80%] max-w-[1400px] mx-auto flex justify-between items-center '>
+            <div className='w-[80%] max-w-[1400px] mx-auto flex max-sm:flex-col max-sm:gap-2 justify-between items-center '>
                 <div className='font-[Preahvihear] text-sm flex items-center gap-2'>
                     <FaRegCopyright />
                     <p>2024 miniURL . Checkout on <span> <a className='hover:text-lg duration-700 transition-all underline' href='https://github.com/adeeb-123/miniURL' target='_blank'>GitHub</a> </span></p>
                 </div>
-                <div className='flex gap-2 text-2xl cursor-pointer'>
-                    <a href='https://github.com/adeeb-123'><FaGithub className='' /></a>
-                    <a href='https://www.linkedin.com/in/adeeb-ahmad-b148951b9/'><FaLinkedin className='' /></a>
-                    <a href='https://twitter.com/adeebsid12345'><FaSquareXTwitter className='' /></a>
+                <div className='flex gap-4 text-2xl cursor-pointer'>
+                    <a href='https://github.com/adeeb-123'><FaGithub className='hover:scale-[1.1] duration-300 transition-all' /></a>
+                    <a href='https://www.linkedin.com/in/adeeb-ahmad-b148951b9/'><FaLinkedin className='hover:scale-[1.1] duration-300 transition-all' /></a>
+                    <a href='https://twitter.com/adeebsid12345'><FaSquareXTwitter className='hover:scale-[1.1] duration-300 transition-all' /></a>
                 </div>
             </div>
         </div>
